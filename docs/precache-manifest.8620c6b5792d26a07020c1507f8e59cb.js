859b2a912c0841b4887820352e1ede5873bd5e80
meikidd <meikidd@gmail.com>
2019-01-18 10:46:50 +0800

    build/ -> docs/

---
+++ docs/precache-manifest.8620c6b5792d26a07020c1507f8e59cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5d0d4687e2d27256230",
+    "url": "/esl-shadowing/static/js/runtime~main.a5d0d468.js"
+  },
+  {
+    "revision": "cd460b12eab9d36a4c76",
+    "url": "/esl-shadowing/static/js/main.cd460b12.chunk.js"
+  },
+  {
+    "revision": "cc68eb2a8cd2447747c0",
+    "url": "/esl-shadowing/static/js/1.cc68eb2a.chunk.js"
+  },
+  {
+    "revision": "cd460b12eab9d36a4c76",
+    "url": "/esl-shadowing/static/css/main.bb16636a.chunk.css"
+  },
+  {
+    "revision": "ef4c2ce1c704c47d1af1f5ee773a1e47",
+    "url": "/esl-shadowing/index.html"
+  }
+];
